e79c62c38a3e3f526250c5702b6480c4708272e6
Tu Phan <tu.phan@tether.io>
2026-04-28 13:06:23 +0700

    fix: update static method

--- src/wallet-account-read-only-tron.js
+++ src/wallet-account-read-only-tron.js
@@ -67,7 +67,7 @@ export default class WalletAccountReadOnlyTron extends WalletAccountReadOnly {
      * @protected
      * @type {TronWeb | undefined}
      */
-    this._tronWeb = this._initializeProvider(config)
+    this._tronWeb = WalletAccountReadOnlyTron._initializeProvider(config)
   }
 
   /**
